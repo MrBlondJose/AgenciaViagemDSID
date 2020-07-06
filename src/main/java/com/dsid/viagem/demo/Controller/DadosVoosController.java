4bf0289ee9c39990af8177b1ec6ccfc82c9b5aaf
MrBlondjose <grupo.startup2016@gmail.com>
2020-07-05 22:09:37 -0300

    Ajustes Api de voos

--- src/main/java/com/dsid/viagem/demo/Controller/DadosVoosController.java
+++ src/main/java/com/dsid/viagem/demo/Controller/DadosVoosController.java
@@ -1,7 +1,11 @@
 package com.dsid.viagem.demo.Controller;
 
+import com.dsid.viagem.demo.DadosHotels.Models.Image;
+import com.dsid.viagem.demo.DadosLocalizacoes.DadosLocalizacoesAeroportosSerivce;
+import com.dsid.viagem.demo.DadosLocalizacoes.DadosLocalizacoesService;
 import com.dsid.viagem.demo.DadosVoos.DadosVoosService;
 import com.dsid.viagem.demo.DadosVoos.model.Voo;
+import com.dsid.viagem.demo.PackageBuilder.models.Airport;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,12 +27,19 @@ public class DadosVoosController {
     private DadosVoosService dadosVoosService;
 
     @Autowired
+    DadosLocalizacoesAeroportosSerivce dadosLocalizacoesAeroportosSerivce;
+
+    @Autowired
+    DadosLocalizacoesService dadosLocalizacoesService;
+
+
+    @Autowired
     private ObjectMapper mapper;
 
     @GetMapping(path="/voos", produces = "application/json")
     public String getDadosVoos(
-            @RequestParam(required = true) String d1,
-            @RequestParam(required = true) String o1,
+            @RequestParam(required = true) String destino,
+            @RequestParam(required = true) String origem,
             @RequestParam(required = true) String dd1,
             @RequestParam(required = false) String dd2,
             @RequestParam(required = false) String o2,
@@ -37,8 +49,10 @@ public class DadosVoosController {
             @RequestParam(required = false) String tc,
             @RequestParam(required = false) String c) throws JsonProcessingException {
         Map<String,String> parameters= new HashMap<String, String>();
-        parameters.put("d1",d1);
-        parameters.put("o1",o1);
+
+
+        parameters.put("d1",this.getNearAirportFromLocation(destino,"200"));
+        parameters.put("o1",this.getNearAirportFromLocation(origem,"200"));
         parameters.put("dd1",dd1);
         if(dd2!=null)parameters.put("dd2",dd2);
         if(o2!=null)parameters.put("o2",o2);
@@ -50,5 +64,54 @@ public class DadosVoosController {
         List<Voo> response= this.dadosVoosService.getExternalFlighData(parameters);
         return mapper.writeValueAsString(response);
     }
+
+
+    public  String getNearAirportFromLocation(String locationQuery, String radius){
+        Map<String,Object> dadosLocation=this.getLocationData(locationQuery);
+        String latitude= this.getLatitude(dadosLocation);
+        String longitude=this.getLongitude(dadosLocation);
+        List<Airport> airports=new ArrayList<>();
+        String locationArrumada=locationQuery.trim().toLowerCase().replaceAll("_","").replaceAll("ã","a").replaceAll(" ","");
+        if(locationArrumada.equals("saopaulo") || locationArrumada.equals("guarulhos")){
+            airports.add( Airport.createGuarulhos());
+            return airports.get(0).getIataCode();
+        }
+        Map<String,Object> aeroportosProximos= this.dadosLocalizacoesAeroportosSerivce.getNearAirportsData(latitude,longitude,radius,"2");
+        int size= ((List<Map>)(aeroportosProximos.get("items"))).size();
+
+        for(int i=0;i<size && i<5;i++){
+            airports.add(new Airport(aeroportosProximos,i,this.getLocationId(dadosLocation)));
+        }
+        return airports.get(0).getIataCode();
+    }
+
+
+    private String getLocationId(Map<String,Object> locationsResponse){
+        List<Map> data= (List<Map>) locationsResponse.get("data");
+        Map<String,Object> resultObject=(Map<String, Object>) (data.get(0).get("result_object"));
+        return (String)resultObject.get("location_id");
+    }
+
+    private Map<String,Object> getLocationData(String locationQuery){
+        Map<String,String> parameters= new HashMap<>();
+        parameters.put("units","km");
+        parameters.put("query",locationQuery);
+        Map<String,Object> map= dadosLocalizacoesService.getLocationsData(parameters);
+        return map;
+    }
+
+    private String getLatitude(Map<String,Object> locationsResponse){
+        List<Map> data= (List<Map>) locationsResponse.get("data");
+        Map<String,Object> resultObject=(Map<String, Object>) (data.get(0).get("result_object"));
+        return (String)resultObject.get("latitude");
+
+    }
+
+    private String getLongitude(Map<String,Object> locationsResponse){
+        List<Map> data= (List<Map>) locationsResponse.get("data");
+        Map<String,Object> resultObject=(Map<String, Object>) (data.get(0).get("result_object"));
+        return (String)resultObject.get("longitude");
+    }
+
 
 }
